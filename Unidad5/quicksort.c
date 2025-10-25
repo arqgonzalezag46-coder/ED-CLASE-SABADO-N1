41e95bf85ce47c4bdacf742b679c34440ee62054
yennypaolabravoparedes42-oss <yennypaolabravoparedes42@gmail.com>
2025-10-25 02:07:44 -0500

    Unidad5

--- Unidad5/quicksort.c
+++ Unidad5/quicksort.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
+
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
+
 int partition(int arr[], int low, int high) {
     int pivot = arr[high];  
-    int i = low;            
+    int i = low;
 
     for (int j = low; j < high; j++) {
         if (arr[j] < pivot) {
@@ -20,22 +22,29 @@ int partition(int arr[], int low, int high) {
     swap(&arr[i], &arr[high]);
     return i;
 }
+
+
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int p = partition(arr, low, high);
-
         quickSort(arr, low, p - 1);
         quickSort(arr, p + 1, high);
     }
 }
 
+
 int main() {
-    int data[] = {6, 2, 4, 1, 5, 8, 10, 3, 9, 7};
+    int data[] = {28, 11, 96, -5, 21, 18, 12, 22};
     int n = sizeof(data) / sizeof(data[0]);
+
+    printf("Arreglo original:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", data[i]);
+    }
 
     quickSort(data, 0, n - 1);
 
-    printf("Arreglo ordenado con QuickSort:\n");
+    printf("\n\nArreglo ordenado con Quicksort:\n");
     for (int i = 0; i < n; i++) {
         printf("%d ", data[i]);
     }
